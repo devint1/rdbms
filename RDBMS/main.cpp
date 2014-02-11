121eb9958b5d29020bd01f6a1f02eb536b3fad35
clement360 <miguelfclement@gmail.com>
2014-02-10 19:52:26 -0600

    Tons of join magic

--- RDBMS/main.cpp
+++ RDBMS/main.cpp
@@ -20,11 +20,14 @@ int main()
 	db.writeTable("users");
 	db.closeTable("users");
 	db.openTable("cars2");
+	db.openTable("students");
+	db.openTable("schoolLocations");
 
 	TableOperations::setUnion(db.findTable("cars"), db.findTable("cars2"), "car_id");
 	TableOperations::setDifference(db.findTable("cars"), db.findTable("cars2"), "car_id");
 	TableOperations::setDifference(db.findTable("cars2"), db.findTable("cars2"), "car_id");
 	TableOperations::setDifference(db.findTable("cars2"), db.findTable("cars"), "car_id");
+	TableOperations::naturalJoin(db.findTable("students"), db.findTable("schoolLocations"));
 	
 	system("PAUSE");
 	return 0;
