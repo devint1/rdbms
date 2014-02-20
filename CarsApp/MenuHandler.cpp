c0007755e9ebcd578c94bff6bfbd8eda4766fddf
Corey Hall <corbob@sbcglobal.net>
2014-02-20 15:59:42 -0600

    commented my cases

--- CarsApp/MenuHandler.cpp
+++ CarsApp/MenuHandler.cpp
@@ -8,21 +8,21 @@ void MenuHandler::printMenu()
 	cout << "1)  Add car" << endl;
 	cout << "2)  Add car location" << endl;
 	cout << "3)  Modify car" << endl;
-	cout << "4)  Modify car location" << endl;
+	cout << "4)  Modify car location" << endl;			//Corey
 	cout << "5)  Delete car" << endl;
 	cout << "6)  Delete car location" << endl;
 	cout << "7)  List all cars" << endl;
-	cout << "8)  List all car locations" << endl;
+	cout << "8)  List all car locations" << endl;		//Corey
 	cout << "9)  List all cars with locations" << endl;
 	cout << "10) Find cars" << endl;
 	//User operations
 	cout << "11) Add user" << endl;
-	cout << "12) Modify user" << endl;
+	cout << "12) Modify user" << endl;					//Corey
 	cout << "13) Delete user" << endl;
 	cout << "14) Show all users" << endl;
 	cout << "15) Find users" << endl;
 	//Utilities
-	cout << "16) Remove duplicates" << endl;
+	cout << "16) Remove duplicates" << endl;			//Corey
 	cout << "17) Display duplicates" << endl;
 	cout << "18) Display all ownership combonations" << endl;
 	cout << "19) Exit program" << endl;
