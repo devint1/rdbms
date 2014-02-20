a616eb1393690fa06bbf0bfc409eed0e887ce215
Sergio Avedil <sergio101@neo.tamu.edu>
2014-02-20 16:01:23 -0600

    Operations

--- CarsApp/MenuHandler.cpp
+++ CarsApp/MenuHandler.cpp
@@ -6,26 +6,26 @@ void MenuHandler::printMenu()
 {
 	//Car operations	
 	cout << "1)  Add car" << endl;
-	cout << "2)  Add car location" << endl;
+	cout << "2)  Add car location" << endl;		//Sergio
 	cout << "3)  Modify car" << endl;
 	cout << "4)  Modify car location" << endl;
 	cout << "5)  Delete car" << endl;
-	cout << "6)  Delete car location" << endl;
+	cout << "6)  Delete car location" << endl;	  //Sergio
 	cout << "7)  List all cars" << endl;
 	cout << "8)  List all car locations" << endl;
 	cout << "9)  List all cars with locations" << endl;
-	cout << "10) Find cars" << endl;
+	cout << "10) Find cars" << endl;	//Sergio
 	//User operations
 	cout << "11) Add user" << endl;
 	cout << "12) Modify user" << endl;
 	cout << "13) Delete user" << endl;
-	cout << "14) Show all users" << endl;
+	cout << "14) Show all users" << endl; //Sergio
 	cout << "15) Find users" << endl;
 	//Utilities
 	cout << "16) Remove duplicates" << endl;
 	cout << "17) Display duplicates" << endl;
-	cout << "18) Display all ownership combonations" << endl;
-	cout << "19) Exit program" << endl;
+	cout << "18) Display all ownership combonations" << endl;	 //Sergio
+	cout << "19) Exit program" << endl;			
 	cout << endl;
 }
 
